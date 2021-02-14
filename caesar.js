4025c6f6d11a008e25fe88b7b934828cdee3637c
Brody Bayley <brodybayley@gmail.com>
2021-02-14 20:53:52 +0000

    pushing through final changes to function to ensure last test case passes

--- caesar.js
+++ caesar.js
@@ -1,19 +1,23 @@
 const encrypt = function(plaintext, key) {
   let finalString = '';
   for (const letter of plaintext) {
-    const newKey = (letter.charCodeAt(0) - 97) + key; {
-      if (newKey < 0) {
-        const newLetter = String.fromCharCode(97 + (26 + newKey));
-        finalString += newLetter;
-      } else {
-        const newLetter = String.fromCharCode(97 + newKey);
-        finalString += newLetter;
-      }
+    const newKey = (letter.charCodeAt(0) - 97) + key;
+    if (letter === ' ') {
+      finalString += ' ';
+    } else if (newKey < 0) {
+      const newLetter = String.fromCharCode(97 + (26 + newKey));
+      finalString += newLetter;
+    } else if (newKey > 26) {
+      const newLetter = String.fromCharCode(97 + (newKey - 26));
+      finalString += newLetter;
+    } else {
+      const newLetter = String.fromCharCode(97 + newKey);
+      finalString += newLetter;
     }
   }
   return finalString;
 };
 
-console.log(encrypt("hello", -3));
+console.log(encrypt("hello world", 5));
 
 module.exports = { encrypt };
